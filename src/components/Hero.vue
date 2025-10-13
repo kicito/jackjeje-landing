f654abc1add44c5600334d5c0b823cca6cc242d5
kicito <narongrit.kie@gmail.com>
2025-10-13 15:43:12 +0200

    Auto stash before rebase of "main"

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -57,7 +57,11 @@ import { images } from '../assets/images.js'
 .hero-background {
   position: absolute;
   top: 0;
+  left: 0;
+  right: 0;
   bottom: 0;
+  width: 100%;
+  height: 100%;
   overflow: hidden;
 }
 
